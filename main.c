d5fe6fc1f78bbdfc12de455d288ea16239ba708b
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2023-07-09 10:46:42 -0400

    fix copyright

--- main.c
+++ main.c
@@ -33,11 +33,7 @@ static void print_help(const char* argv[]) {
 
 static void print_version(void) {
     printf("Copyright (C) 2021 Ethan Uppal\n"
-           "License GPLv3+: GNU GPL version 3 or later "
-           "<https://gnu.org/licenses/gpl.html>\n"
-           "This is free software: you are free to change and redistribute "
-           "it.\n"
-           "There is NO WARRANTY, to the extent permitted by law.\n");
+           "All rights reserved.\n");
 }
 
 int main(int argc, const char* argv[]) {
